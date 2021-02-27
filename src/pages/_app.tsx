be93265dff3c6002342836731d2410b995b05500
VictorGuiVasco <victorguilhermecavalcante@gmail.com>
2021-02-26 21:46:16 -0300

    added integration with cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,8 @@
-import { ChallengesProvider } from  '../contexts/ChallengeContext'
-
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider >
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
